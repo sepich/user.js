c754d835bbc73d9ce141aab30fec76c1e29f272a
sepich <i@sepa.spb.ru>
2014-09-07 20:09:21 +0400

    initial upload

---
+++ fp.user.js
@@ -0,0 +1,100 @@
+// ==UserScript==
+// @name        FP
+// @description Minor Footprints improvements
+// @namespace   sepa.spb.ru
+// @version     2014.07.21
+// @include     https://footprints.intermedia.net/MRcgi/MRTicketPage.pl*
+// @icon        https://footprints.intermedia.net/MRimg/uni.ico
+// @grant       unsafeWindow
+// @run-at      document-end
+// @updateURL   https://i.sepa.spb.ru/_/imedia/fp.user.js
+// @downloadURL https://i.sepa.spb.ru/_/imedia/fp.user.js
+// @author      i@sepa.spb.ru
+// ==/UserScript==
+console.log('started');
+var   $,
+    	setTimeoutCount = 0,
+    	setTimeoutCountMax = 60000,
+    	setTimeoutDelay = 500,
+    	hereDoc;
+
+// a function that loads jQuery and calls a callback function when jQuery has finished loading
+function addJQuery(callback) {
+  var script = document.createElement("script");
+  script.setAttribute("src", "//code.jquery.com/jquery-2.1.0.min.js");
+  script.addEventListener('load', function() {
+    var script = document.createElement("script");
+    script.textContent = "window.jQ=jQuery.noConflict(true);(" + callback.toString() + ")();";
+    document.body.appendChild(script);
+  }, false);
+  document.body.appendChild(script);
+}
+
+// the guts of this userscript
+function main() {
+	function hereDoc(f) { return f.toString().replace(/^[^\/]+\/\*!?/, '').replace(/\*\/[^\/]+$/, ''); }
+  var css = hereDoc(function() {/*!
+  textarea { resize: both;}
+  #esc_notes{
+   height: 300px;
+   overflow: auto;
+   border: 1px solid #AAA;
+   width: 90%;
+   white-space: pre-line;
+  }
+  b.esc_user {
+   border-top: 1px solid #CCC;
+   display: block;
+  }
+  */});
+
+  jQ('head').append('<style type="text/css" id="tbl-css">');
+  jQ('#tbl-css').html(css);    
+	jQ('#assgnee').css('height','300px');
+	jQ('#pmember').css('height','300px');
+    
+  //change control editing
+  if(jQ('select#Impacted__bServices').length){
+  	jQ('select#Impacted__bServices').css('height','300px');
+      jQ('select#Impacted__bProduction__bUnit').css('height','300px');
+  }
+  
+  //view case
+  if(jQ('div#ESC__bNotes textarea').length){
+    console.log('view case');
+  	var t=jQ('div#ESC__bNotes textarea'),
+        esc="\n"+jQ('input#ESC__bNotes').val();
+      
+    if(jQ('input#Account__bNotes').length){
+      notes=jQ('input#Account__bNotes').val().replace(/\r\n|\n/g,'<br>');
+      jQ('div#Account__bNotes').html(notes);
+    }        
+    esc=esc.replace(/<!--defang_/g,'&lt;');
+    esc=esc.replace(/</g,'&lt;');
+    esc=esc.replace(/-->/g,'&gt;');
+    esc=esc.replace(/>/g,'&gt;');
+    esc=esc.replace(/defang_@/g,'@');
+    esc=esc.replace(/\n(Entered on [0-9\-]+ at [0-9\:]+ by .*?)\n/mg,"\n<b class='esc_user'>\$1</b>");
+    esc=esc.replace(/\r\n|\n/g,'<br>');
+    t.css('display','none');
+    jQ('label#ESC__bNotes_label').css('display','none');
+    jQ('div#ESC__bNotes').after('<div id="esc_note">'+esc+'</div>');
+  }
+  
+  //edit case
+  if(jQ('#ESC__bNotes_originalData').length){
+  	var esc="\n"+jQ('#ESC__bNotes_originalData').val();
+    esc=esc.replace(/<!--defang_/g,'&lt;');
+    esc=esc.replace(/</g,'&lt;');
+    esc=esc.replace(/-->/g,'&gt;');
+    esc=esc.replace(/>/g,'&gt;');
+    esc=esc.replace(/defang_@/g,'@');
+    esc=esc.replace(/\n(Entered on [0-9\-]+ at [0-9\:]+ by .*?)\n/mg,"<b class='esc_user'>\$1</b>");
+    esc=esc.replace(/\r\n|\n/g,'<br>');
+    jQ('textarea#ESC__bNotes').css('width','90%');
+  	jQ('div#ESC__bNotes_originalDataDiv').parent('div').append('<div id="esc_notes">'+esc+'</div>');
+  }
+}
+
+// load jQuery and execute the main function
+addJQuery(main);
