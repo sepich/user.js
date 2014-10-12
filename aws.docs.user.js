4b77167297de308c4f08760bbfd26f661a408adf
sepich <i@sepa.spb.ru>
2014-10-12 19:10:26 +0400

    filter for images

--- aws.docs.user.js
+++ aws.docs.user.js
@@ -4,9 +4,10 @@
 // @namespace   sepa.spb.ru
 // @version     2014.10.12
 // @include     http://docs.aws.amazon.com/*
-// @include     https://*.docs.aws.amazon.com/*
+// @include     https://docs.aws.amazon.com/*
 // @icon        http://media.amazonwebservices.com/favicon.ico
 // @grant       GM_getResourceText
+// @updateURL   https://openuserjs.org/install/sepich/aws.docs.user.js
 // @require     http://code.jquery.com/jquery-latest.min.js
 // @author      i@sepa.spb.ru
 // ==/UserScript==
@@ -27,6 +28,11 @@ var css = hereDoc(function() {/*!
   column-gap: 30px;
   text-align: justify;
 }
+
+.d-col p {
+  margin: 3px 0;
+  text-indent: 20px;
+}
 */}); 
 
 //apply custom CSS
@@ -35,8 +41,23 @@ s.type = 'text/css';
 s.innerHTML = css;
 document.documentElement.appendChild(s);
 
-var n=$('h2.title').closest('div.titlepage');
+var d,
+    n=$('h1.topictitle, h2.title').closest('div.titlepage');
 for(var i=0; i<n.length; i++){
   $(n[i]).after("<div class='d-col'/>");
-  $("div.d-col").last().append( $(n[i]).siblings() );
+  d=$("div.d-col").last();
+  
+  $(n[i]).siblings().each(function(){
+         if(this.nodeName=="DIV" && $(this).hasClass('section') && $(this).has('h2').length ) return false; //break
+    else if(this.nodeName=="DIV" && $(this).hasClass('d-col'))   return true;  //continue
+    else if(this.nodeName=="TABLE" || 
+           (this.nodeName=="DIV" && ( $(this).hasClass('mediaobject') || $(this).has('img').length ))           
+           ){
+      $(this).after("<div class='d-col'/>");
+      d=$("div.d-col").last();
+    }
+    else {
+      $(d).append(this);
+    }
+  }); 
 }
