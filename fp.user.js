cd05d5b6d7a508345c3c62b79b0e8e61095fa06c
sepich <i@sepa.spb.ru>
2014-10-19 12:09:25 +0400

    bigfixes for GIRR, button to scroll to Description

--- fp.user.js
+++ fp.user.js
@@ -2,7 +2,7 @@
 // @name        FP
 // @description Minor Footprints improvements
 // @namespace   sepa.spb.ru
-// @version     2014.10.12
+// @version     2014.10.19
 // @include     https://footprints.intermedia.net/MRcgi/MRTicketPage.pl*
 // @icon        https://footprints.intermedia.net/MRimg/uni.ico
 // @grant       unsafeWindow
@@ -47,21 +47,51 @@ function main() {
      display: block;
     }
     u { font-family: monospace; text-decoration:none;}
+    label#Root__bCause_label {margin-top: 20px;}
+    .ctlbut { 
+      padding: 3px 4px;
+      color: #006699;
+    }
+    .ctlbut:hover { 
+      border: 2px outset threedhighlight;
+      padding: 1px 2px;
+      color: #006699;
+    }    
   */});
-
+  
+  //htmlizer for plain text in escalation notes
+  function escnotes(txt){
+    txt=txt.replace(/<!--defang_/g,'&lt;');
+    txt=txt.replace(/</g,'&lt;');
+    txt=txt.replace(/-->/g,'&gt;');
+    txt=txt.replace(/>/g,'&gt;');
+    txt=txt.replace(/defang_@/g,'@');
+    txt=txt.replace(/\n(Entered on [0-9\-]+ at [0-9\:]+ by .*?)\n/mg,"\n<b class='esc_user'>\$1</b>");
+    txt=txt.replace(/\r\n|\n/g,'<br>');
+    txt=txt.replace(/(http[s]?:\/\/[^ )\n\r"<>]+)/g,'<a href="'+"$1"+'" target="_blank">'+"$1</a>");
+    txt=txt.replace(/ (gid:)(\S+) /g,' <a href="http://eiger.accessline.com/sw/SmartWatcher.html?type=gid&gid='+"$2"+'&internal=true" target="_blank">'+"$1</a> <u>$2</u> ");
+    return txt;
+  }
+  
   jQ('head').append('<style type="text/css" id="tbl-css">');
-  jQ('#tbl-css').html(css);    
+  jQ('#tbl-css').html(css);
   jQ('#assgnee').css('height','300px');
   jQ('#pmember').css('height','300px');
     
   //change control editing
   if(jQ('select#Impacted__bServices').length){
+    console.log('edit change control');
     jQ('select#Impacted__bServices').css('height','300px');
     jQ('select#Impacted__bProduction__bUnit').css('height','300px');
   }
-  
+
+  //change control view
+  if(jQ('div#Technical__bInformation').length){
+    console.log('view change control');
+  }  
+
   //view case
-  if(jQ('div#ESC__bNotes textarea').length){
+  else if(jQ('div#ESC__bNotes textarea').length){
     var id;
     console.log('view case');
     if(jQ('input#Account__bNotes').length){
@@ -78,85 +108,39 @@ function main() {
     });    
     
     var t=jQ('div#ESC__bNotes textarea'),
-        esc="\n"+jQ('input#ESC__bNotes').val();       
-    esc=esc.replace(/<!--defang_/g,'&lt;');
-    esc=esc.replace(/</g,'&lt;');
-    esc=esc.replace(/-->/g,'&gt;');
-    esc=esc.replace(/>/g,'&gt;');
-    esc=esc.replace(/defang_@/g,'@');
-    esc=esc.replace(/\n(Entered on [0-9\-]+ at [0-9\:]+ by .*?)\n/mg,"\n<b class='esc_user'>\$1</b>");
-    esc=esc.replace(/\r\n|\n/g,'<br>');
-    esc=esc.replace(/(http[s]?:\/\/[^ )\n\r"<>]+)/g,'<a href="'+"$1"+'" target="_blank">'+"$1</a>");
-    esc=esc.replace(/ (gid:)(\S+) /g,' <a href="http://eiger.accessline.com/sw/SmartWatcher.html?type=gid&gid='+"$2"+'&internal=true" target="_blank">'+"$1</a> <u>$2</u> ");
+        esc="\n"+jQ('input#ESC__bNotes').val();
+    esc=escnotes(esc);
     t.css('display','none');
     jQ('label#ESC__bNotes_label').css('display','none');
-    jQ('div#ESC__bNotes').after('<div id="esc_note">'+esc+'</div>');
+    jQ('div#ESC__bNotes').after('<div id="esc_note">'+esc+'</div>');    
+    jQ('div#reportButton').closest('table').parent().after('<td><a class="ctlbut" href="#Description_ecHeading">Description</a></td>');    
   }  
-  //or view archive case
-  else if(jQ('div#ESC__bNotes input#ESC__bNotes').length){
-    console.log('view archive case');
-    if(jQ('input#Account__bNotes').length){
-      var notes=jQ('input#Account__bNotes').val().replace(/\r\n|\n/g,'<br>');
-      jQ('div#Account__bNotes').html(notes);
-    }
-    jQ('div#HP__bUsername').contents().each(function() {
-      if(this.nodeType == 3) {
-        jQ(this).after(')');
-        if( /^S[EW]H$/.test(jQ('#HP__bProduct').text()) ) jQ(this).after('/<a href="https://hosting.intermedia.net/asp/User/LoginToAccount.asp?accountID='+jQ('#Organization').text()+'" target="_blank">Login</a>');        
-        if(/al-[0-9]+/.test(jQ(this).text()) ) jQ(this).after('/<a href="https://eiger.accessline.com/atlas/page/enterprise/lf/view/'+jQ(this).text().replace(/^al-/,'')+'" target="_blank">Atlas</a>');
-        jQ(this).after(' (<a href="https://hosting.intermedia.net/asp/Administrator/ViewAccounts.asp?Where=Accounts.userName%20LIKE%20%27'+jQ(this).text()+'%25%27" target="_blank">HP</a>');
-      }
-    });
-    
-    var t=jQ('div#ESC__bNotes'),
-        esc="\n"+jQ('input#ESC__bNotes').val();       
-    esc=esc.replace(/<!--defang_/g,'&lt;');
-    esc=esc.replace(/</g,'&lt;');
-    esc=esc.replace(/-->/g,'&gt;');
-    esc=esc.replace(/>/g,'&gt;');
-    esc=esc.replace(/defang_@/g,'@');
-    esc=esc.replace(/\n(Entered on [0-9\-]+ at [0-9\:]+ by .*?)\n/mg,"\n<b class='esc_user'>\$1</b>");
-    esc=esc.replace(/\r\n|\n/g,'<br>');
-    esc=esc.replace(/(http[s]?:\/\/[^ )\n\r"<>]+)/g,'<a href="'+"$1"+'" target="_blank">'+"$1</a>");
-    esc=esc.replace(/ (gid:)(\S+) /g,' <a href="http://eiger.accessline.com/sw/SmartWatcher.html?type=gid&gid='+"$2"+'&internal=true" target="_blank">'+"$1</a> <u>$2</u> ");
-    t.css('display','none');
-    jQ('label#ESC__bNotes_label').css('display','none');
-    jQ('div#ESC__bNotes').after('<div id="esc_note">'+esc+'</div>');
-  }
-  
+ 
   //edit case
-  if(jQ('#ESC__bNotes_originalData').length){
+  else if(jQ('#ESC__bNotes_originalData').length){
     console.log('edit case');
     var esc="\n"+jQ('#ESC__bNotes_originalData').val();
-    esc=esc.replace(/<!--defang_/g,'&lt;');
-    esc=esc.replace(/</g,'&lt;');
-    esc=esc.replace(/-->/g,'&gt;');
-    esc=esc.replace(/>/g,'&gt;');
-    esc=esc.replace(/defang_@/g,'@');
-    esc=esc.replace(/\n(Entered on [0-9\-]+ at [0-9\:]+ by .*?)\n/mg,"<b class='esc_user'>\$1</b>");
-    esc=esc.replace(/\r\n|\n/g,'<br>');
-    esc=esc.replace(/(http[s]?:\/\/[^ )\n\r"<>]+)/g,'<a href="'+"$1"+'" target="_blank">'+"$1</a>");
-    esc=esc.replace(/ (gid:)(\S+) /g,' <a href="http://eiger.accessline.com/sw/SmartWatcher.html?type=gid&gid='+"$2"+'&internal=true" target="_blank">'+"$1</a> <u>$2</u> ");
+    esc=escnotes(esc);
     jQ('textarea#ESC__bNotes').css('width','90%');
     jQ('div#ESC__bNotes_originalDataDiv').parent('div').append('<div id="esc_notes">'+esc+'</div>');
+    jQ('div#ESC__bNotes_originalDataDiv').css('display','none');
   }
   
   //view GIRR
-  if(jQ('#Root__bCause').length){
+  else if(jQ('#Root__bCause').length || jQ('div#Notes textarea').length){
     console.log('view GIRR');
+    //notes
     var t=jQ('div#Notes textarea'),
-        esc="\n"+jQ('input#Notes').val();       
-    esc=esc.replace(/<!--defang_/g,'&lt;');
-    esc=esc.replace(/</g,'&lt;');
-    esc=esc.replace(/-->/g,'&gt;');
-    esc=esc.replace(/>/g,'&gt;');
-    esc=esc.replace(/defang_@/g,'@');
-    esc=esc.replace(/\n(Entered on [0-9\-]+ at [0-9\:]+ by .*?)\n/mg,"\n<b class='esc_user'>\$1</b>");
-    esc=esc.replace(/\r\n|\n/g,'<br>');
-    esc=esc.replace(/(http[s]?:\/\/[^ )\n\r"<>]+)/g,'<a href="'+"$1"+'" target="_blank">'+"$1</a>");
-    esc=esc.replace(/ (gid:)(\S+) /g,' <a href="http://eiger.accessline.com/sw/SmartWatcher.html?type=gid&gid='+"$2"+'&internal=true" target="_blank">'+"$1</a> <u>$2</u> ");
+        esc="\n"+jQ('input#Notes').val();
+    esc=escnotes(esc);
     t.css('display','none');
-    jQ('div#Notes').after('<div id="esc_note">'+esc+'</div>');   
+    jQ('div#Notes').after('<div id="esc_note">'+esc+'</div>'); 
+    //rootcause
+    t=jQ('div#Root__bCause textarea');
+    esc="\n"+jQ('input#Root__bCause').val();
+    esc=escnotes(esc);
+    t.css('display','none');
+    jQ('div#Root__bCause').after('<div id="esc_root">'+esc+'</div>');
   }
 }
 
